025987ea6a3114451d56de555cc1161789aff5d2
meleadr <meledina.adriano@gmail.com>
2023-02-21 20:26:07 +0100

    Changement du sendmail

--- sendmail.php
+++ sendmail.php
@@ -1,19 +1,17 @@
 <?php
 
-if(isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $mailFrom = $_POST['email'];
-  $message = $_POST['message'];
-  $phone = $_POST['phone'];
+$name = $_POST['name'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message'];
+$phone = $_POST['phone'];
 
-  $mailTo = "monadresse@domaine.com";
-  $subject = "Elisaservices - Contact";
-  $headers = "De: ".$mailFrom;
-  $message = $message."\n\n".$phone;
-  $txt = "Vous avez reçu un message de ".$name.".\n\n".$message;
+$mailTo = "elisaservices@hotmail.com";
+$subject = "Elisaservices - Contact";
+$headers = "De: ".$mailFrom;
+$message = $message."\n\n".$phone;
+$txt = "Vous avez reçu un message de ".$name.".\n\n".$message;
 
-  mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.html");
-}
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.html");
 
 ?>
